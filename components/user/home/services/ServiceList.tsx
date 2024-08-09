6a3fa7200ab4826778772afb0f3ad4d13b15e6f3
bekidink <bereketdinku876@gmail.com>
2024-08-08 23:38:39 -0700

    service list

--- components/user/home/services/ServiceList.tsx
+++ components/user/home/services/ServiceList.tsx
@@ -6,7 +6,7 @@ export default function ServiceList({data}:{data:ServiceProps[]}) {
   return (
     <div className='grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6'>
       {data.map((service,i)=>(
-        <ServiceCard service={service}/>
+        <ServiceCard service={service} key={i}/>
       ))}
     </div>
   )
